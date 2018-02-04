ebb5d8eaead28b62ebed22382a3ad2f606c2ef50
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-04 18:34:40 +0330

    initial commit

---
+++ myFirstTrainings.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.0;
+
+import "./safeMath.sol";
+
+
+contract Coin {
+    using SafeMath for uint;
+    mapping(address => uint) public _balanceOF;
+    address public _owner;
+    uint public constant TOTAL_SUPPLY = 1000;
+    event Sent(address _from, address _to, uint _value);
+
+    function Coin() public {
+        _owner = msg.sender; 
+        _balanceOF[msg.sender] = TOTAL_SUPPLY; 
+    }
+
+    function transferTO(address _to, uint _value) public returns(bool success) {
+        if (_owner == msg.sender) {
+            if (balanceOf(msg.sender) >= _value && _value > 0 && balanceOf(msg.sender) > 0) {
+                _balanceOF[msg.sender].sub(_value);
+                _balanceOF[_to].add(_value);
+                Sent(msg.sender, _to, _value);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns(bool success) {
+        if (balanceOf(_from) >= _value && _value > 0 && balanceOf(_from) > 0) {
+            _balanceOF[_from].sub(_value);
+            _balanceOF[_to].add(_value);
+            Sent(_from, _to, _value);
+            return true;
+        }
+
+        return false;
+    }
+
+    function balanceOf(address _address) public returns(uint value) {
+        return _balanceOF[_address];
+    }
+}
